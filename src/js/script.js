4e37ead89aa4c549d01887dee5aa08a0b79e9af5
KatieLuzina <39306742+KatieLuzina@users.noreply.github.com>
2020-02-03 15:51:26 +0500

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -18,14 +18,32 @@ $(document).ready(function(){
     
     $('.general_cards_item_wrapper_button').click(function (e) {
         
-        let id = (e.target.id).toString().slice(-1);
-        for (let i = 1; i < 5; i++) {
-            document.getElementById('more_' + i).classList.remove("general_cards_item_more_active");
-            document.getElementById('item_' + i).classList.remove("general_cards_item_active");
-        }
-        document.getElementById('more_' + id).classList.add("general_cards_item_more_active");
-        document.getElementById('item_' + id).classList.add("general_cards_item_active");
+        let id = (e.target.id).toString().slice(-1);        
+        let activated = document.getElementById('more_' + id).classList.contains("general_cards_item_more_active");
+        if (activated) {
+            for (let i = 1; i < 5; i++) {
+                document.getElementById('more_' + i).classList.remove("general_cards_item_more_active");
+                document.getElementById('item_' + i).classList.remove("general_cards_item_active");
+                document.getElementById('cards').classList.remove("general_cards_active_" + i);
+                document.getElementById('item_' + i).classList.remove("general_cards_item_blured");
 
-        
+            }
+            
+        }
+        else {
+            for (let i = 1; i < 5; i++) {
+                document.getElementById('more_' + i).classList.remove("general_cards_item_more_active");
+                document.getElementById('item_' + i).classList.remove("general_cards_item_active");
+                document.getElementById('item_' + i).classList.add("general_cards_item_blured");
+                document.getElementById('cards').classList.remove("general_cards_active_" + i);
+                let zIndex=Math.abs(4-Math.abs(id-i));
+                document.getElementById('item_' + i).style.zIndex=zIndex;
+            }
+            document.getElementById('more_' + id).classList.add("general_cards_item_more_active");
+            document.getElementById('item_' + id).classList.add("general_cards_item_active");
+            document.getElementById('cards').classList.add("general_cards_active_" + id);
+            document.getElementById('item_' + id).classList.remove("general_cards_item_blured");
+            
+        }        
     })
 })
